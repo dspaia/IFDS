500606b6878776714577d4eaa804317d289ad73f
dspaia <campos.paiva@aluno.ifsp.edu.br>
2024-09-04 10:39:23 -0300

    texto

---
+++ angulo.py
@@ -0,0 +1,5 @@
+from math import sin, cos, tan, radians
+
+a = int(input("Digite o ângulo:"))
+
+print ("Seno: {}\nCosseno: {}\nTangente: {}" .format(sin(radians(a)), cos(radians(a)), tan(radians(a))))
